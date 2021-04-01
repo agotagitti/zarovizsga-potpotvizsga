e1c5c3d0e989ce992b3013d49c8bd78216d31317
agotagitti <agota.subs@gmail.com>
2021-04-01 15:23:48 +0200

    Zarovizsga-potpotvizsga - Fish

---
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/aquarium/Fish.java
@@ -0,0 +1,41 @@
+package hu.nive.ujratervezes.zarovizsga.aquarium;
+
+public abstract class Fish {
+
+    private String name;
+    private int weight;
+    private String color;
+    protected boolean memoryLoss;
+
+    public Fish(String name, int weight, String color) {
+        this.name = name;
+        this.weight = weight;
+        this.color = color;
+    }
+
+    String status() {
+        return name + ", weight: " + weight + ", color: " + color + ", short-term memory loss: " + memoryLoss;
+    }
+
+    abstract void feed();
+
+    public void addGram(int gram) {
+        this.weight += gram;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public boolean hasMemoryLoss() {
+        return memoryLoss;
+    }
+}
---
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/aquarium/Tang.java
@@ -0,0 +1,15 @@
+package hu.nive.ujratervezes.zarovizsga.aquarium;
+
+public class Tang extends Fish {
+
+
+    public Tang(String name, int weight, String color) {
+        super(name, weight, color);
+        memoryLoss = true;
+    }
+
+    @Override
+    void feed() {
+        addGram(1);
+    }
+}
